cd276b24cd22387989dc41907c41ec22f9ffbb1a
kimondo88 <kimondo1988@o2.pl>
2022-11-09 12:24:40 +0100

    add randomTime func, also made change how func is executed

--- index.js
+++ index.js
@@ -3,12 +3,10 @@
 var robot = require("robotjs");
 // Timeout to wait if system is slow
 
-const {Math} = require("Math");
-
 const actions = {
     patrol: [-1515, 649],
     pclaim: [-1170, 656],
-    instantpatrol: [-811, 805],
+    instantpatrol: [-825, 804],
     ipclaim: [-1021, 608],
     ipclaimbutton: [-1158, 774],
     mailicon: [-551, 119],
@@ -16,8 +14,10 @@ const actions = {
     mailexit: [-1801 , 110],
 }
 
+
 setTimeout(startOPM, 1000);
- 
+setTimeout(startInstantPatrol, 3000);
+
 //Opening the openboard
 //Can learn more about these
 //properties from the robotjs site
@@ -28,10 +28,7 @@ function startOPM(){
     setTimeout(()=>{
         robot.moveMouse(actions.pclaim[0], actions.pclaim[1]);
         robot.mouseClick();
-    }, randomTime(50, 1500))
-    
-    //Start sublime text after 1s
-    setTimeout(startInstantPatrol, randomTime(50, 500));
+    }, randomTime(50, 800))
 };
 
 function startInstantPatrol(){
@@ -40,13 +37,13 @@ function startInstantPatrol(){
     setTimeout(()=>{
         robot.moveMouse(actions.ipclaim[0], actions.ipclaim[1]);
         robot.mouseClick();
-    }, randomTime(10, 350))
+    }, randomTime(10, 750))
     setTimeout(()=>{
         robot.moveMouse(actions.ipclaimbutton[0], actions.ipclaimbutton[1]);
         robot.mouseClick();
-    }, randomTime(30, 260))
+    }, randomTime(30, 1250))
 };
 
-function random(max, time){
+function randomTime(max, time){
     return time + Math.floor(Math.random() * max);
 }
